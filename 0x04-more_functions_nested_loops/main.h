6791a72b40d170987e2629ee13a82aab2aac63c2
habiba <habibasangarimohammed@gmail.com>
2022-12-15 15:00:28 -0800

    hello

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -14,6 +13,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-
-#endif 
+#endif
